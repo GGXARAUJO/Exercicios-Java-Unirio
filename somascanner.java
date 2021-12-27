658c9e4aaca0acd6da3077865cdad8b75726e4b8
ggxaraujo <ggxaraujo@gmail.com>
2021-12-26 23:26:36 -0300

    Soma scanner java

---
+++ somascanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String [] args) {
+    int idade;
+    int vasco;
+    Scanner teclado = new Scanner(System.in);
+
+    System.out.println("Qual a sua idade ? ");
+    idade = teclado.nextInt();
+
+    vasco = idade + 10;
+    System.out.printf("Idade = %d\n", vasco);
+    
+}
+}
